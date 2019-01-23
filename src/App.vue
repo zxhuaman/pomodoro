b6ea410ca4ec84786bfc5cf1f88eb5c8c89ed7dc
zxhuaman <zxhuaman@gmail.com>
2019-01-23 19:10:54 +0800

    add:首次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <nav id="nav">
+            <a href="#"><img src="./assets/logo.png" alt="logo"></a>
+            <p class="site-title">promodoro</p>
+            <el-button class="sign-in" type="primary" size="small">登陆</el-button>
+        </nav>
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+    }
+
+    #nav {
+        margin-top: 1.5em;
+        text-align: left;
+    }
+
+    #nav img {
+        width: 2em;
+        margin-left: 2em;
+    }
+
+    .site-title {
+        position: absolute;
+        top: 0;
+        left: 3.5em;
+        height: 100%;
+        font-size: 1.5rem;
+        color: #2c2c2c;
+        text-align: center;
+        padding-bottom: 2em;
+        font-weight: bold;
+    }
+
+    .sign-in {
+        position: absolute;
+        right: 2em;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
+import '../theme/index.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
